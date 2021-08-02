3d4fe9a227dabcdb9a5f3f9be50345d537f22b9c
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2021-08-02 21:58:54 +0200

    refactured

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vkuklys <vkuklys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 18:18:05 by vkuklys           #+#    #+#             */
-/*   Updated: 2021/07/28 22:19:34 by vkuklys          ###   ########.fr       */
+/*   Updated: 2021/08/02 21:31:21 by vkuklys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <errno.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 int		ft_strlen(const char *str);
 int		ft_strncmp(const char *str1, const char *str2, unsigned int n);
 char	*ft_strdup(const char *str1);
 void	*ft_calloc(unsigned int num, unsigned int size);
-char	*ft_strjoin(char **s1, char const *s2);
+char	*ft_strjoin(char **s1, const char *s2);
 
 #endif
